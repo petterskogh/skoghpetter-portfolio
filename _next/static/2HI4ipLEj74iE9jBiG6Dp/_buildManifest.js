033046e3e58fd9a6e2263c5f4b73706a5c148385
petterskogh <51520909+petterskogh@users.noreply.github.com>
2021-06-06 19:26:26 +0000

    Deploying to gh-pages from @ petterskogh/skoghpetter-portfolio@5a638d9d221f2535886f3decd79981628e4f2b3d 🚀

---
+++ _next/static/2HI4ipLEj74iE9jBiG6Dp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/277-f21d438f19c4cb1aaf28.js","static/chunks/pages/index-224610560d12f4cb3f3f.js"],"/_error":["static/chunks/pages/_error-d36babdecdd567a38448.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/zGScXi4mdFfr4iQVpgMio/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/277-f21d438f19c4cb1aaf28.js","static/chunks/pages/index-7e4a923d383b745a3ab4.js"],"/_error":["static/chunks/pages/_error-d36babdecdd567a38448.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
