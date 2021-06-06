5a638d9d221f2535886f3decd79981628e4f2b3d
Petter Skogh <skoghpetter@gmail.com>
2021-06-06 21:25:24 +0200

    La till prefixes, next config och env

--- components/startscreen/startscreen-view.tsx
+++ components/startscreen/startscreen-view.tsx
@@ -8,6 +8,8 @@ type Props = {
   setScroll: React.Dispatch<React.SetStateAction<number>>
 }
 
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 export const StartScreen: React.FC<Props> = ({landscape, scroll, setScroll}) => {
 
   const styleFadeIn = useSpring({ from: {opacity: 0}, to: {opacity: 1}, config: { duration: 500 }  })
@@ -50,7 +52,7 @@ export const StartScreen: React.FC<Props> = ({landscape, scroll, setScroll}) => 
     >
 
       <div className={`h-full flex ${!landscape ? 'flex-col w-full' : 'flex-row-reverse w-2/3'} justify-center items-center`}>
-        <animated.img src="/assets/images/Me.png" alt="Petter Skogh as a 3D character" className={`${landscape ? 'w-3/6' : 'w-5/6'}`} style={{...styleFadeIn}}></animated.img>
+        <animated.img src={prefix + "/assets/images/Me.png"} alt="Petter Skogh as a 3D character" className={`${landscape ? 'w-3/6' : 'w-5/6'}`} style={{...styleFadeIn}}></animated.img>
         <animated.div className="flex flex-col items-center" style={{...styleFadeIn}}>
           <div className={`font-raleway font-bold ${landscape ? 'text-7xl' : 'text-6xl'} text-pwhite mb-4`}>
             <animated.h1 className="font-extrabold text-transparent bg-clip-text bg-gradient-to-b from-pwhite via-pwhite" style={{...styleFromLeft}}>SKOGH</animated.h1>
