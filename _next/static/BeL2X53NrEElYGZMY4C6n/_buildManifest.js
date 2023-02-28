6b09fdc4c4f6ef96994a1ac899bfad497ce4ffcd
petterskogh <51520909+petterskogh@users.noreply.github.com>
2023-02-28 12:24:21 +0000

    Deploying to gh-pages from @ petterskogh/skoghpetter-portfolio@2e4905818f5c2bcc4e42cb149ddaef6f69b667c9 🚀

---
+++ _next/static/BeL2X53NrEElYGZMY4C6n/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/82-839164118caf33b1.js","static/chunks/pages/index-1bcff05bd686113b.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
